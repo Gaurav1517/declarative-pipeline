376d96fb9d2fcee963eb04a1c5ddbd5829c153e5
Gaurav1517 <145908403+Gaurav1517@users.noreply.github.com>
2024-10-08 13:37:29 +0530

    Update and rename Calculator.java to Hello.java

---
+++ myweb/src/main/java/in/javahome/myweb/controller/Hello.java
@@ -0,0 +1,11 @@
+package in.javahome.myweb.controller;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // Print "Hello from Java"
+        System.out.println("Hello from Java");
+    }
+
+    // Other methods can remain as they are or be removed if not needed
+}

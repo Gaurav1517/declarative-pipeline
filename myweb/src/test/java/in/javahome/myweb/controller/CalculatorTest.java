ed714683796a895fdf99d62bd52bce6d98551141
Gaurav1517 <145908403+Gaurav1517@users.noreply.github.com>
2024-10-08 13:43:28 +0530

    Update CalculatorTest.java

--- myweb/src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ myweb/src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -1,17 +1,13 @@
 package in.javahome.myweb.controller;
 
-import junit.framework.Assert;  // Change to JUnit 4 imports
-import junit.framework.TestCase; // Change to JUnit 4 imports
+import junit.framework.TestCase;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest extends TestCase {
-
     @Test
     public void testAddition() {
-        int a = 5;
-        int b = 10;
-        int expected = 15;
-        int result = a + b;
-        Assert.assertEquals(expected, result);
+        // Example assertion
+        assertEquals(2, 1 + 1);
     }
 }
